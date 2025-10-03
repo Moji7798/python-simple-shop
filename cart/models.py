79312c4404095c5da2b687581abf6e7fe3c4c1fe
mojtaba <mojtaba.mosalman.7798@gmail.com>
2025-10-03 17:31:09 +0330

    Day 4: Shopping cart implemented, add/update/remove items, cart view for logged-in users

---
+++ cart/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+from django.contrib.auth.models import User
+from products.models import Product
+
+class Cart(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='cart')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def total_items(self):
+        return sum(item.quantity for item in self.items.all())
+
+    def total_price(self):
+        return sum(item.quantity * item.product.price for item in self.items.all())
+
+    def __str__(self):
+        return f"Cart ({self.user.username})"
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name='items')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+
+    def subtotal(self):
+        return self.quantity * self.product.price
+
+    def __str__(self):
+        return f"{self.product.name} x {self.quantity}"
